58188dfd9b67c8be65996ca1b8d7ede87b9f792d
Brett Miller <brett.miller47@yahoo.com>
2022-10-29 18:53:39 -0400

    added mutations.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,43 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!){
+    login(email: $email, password: $password){
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser(username: $username, email: $email, password: $password){
+    token
+    user {
+      _id
+      username
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($userId: ID!, $book: String!) {
+    saveBook(userId: $userId, book: $book){
+      _id
+      username
+      savedBooks
+    }
+  }
+`;
+
+export const DELETE_BOOK = gql`
+  mutation deleteBook($book: String!) {
+    deleteBook(book: $book) {
+      _id
+      username
+      savedBooks
+    }
+  }
+`;
